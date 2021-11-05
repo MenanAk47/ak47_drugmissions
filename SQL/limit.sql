fdfbde572fcf1163bfe19239c9ecd7d00b21b718
MenanAk47 <35970476+MenanAk47@users.noreply.github.com>
2021-11-05 18:28:19 +0600

    update sql

--- SQL/limit.sql
+++ SQL/limit.sql
@@ -1,5 +1,9 @@
-INSERT INTO `items` (name, label, limit, rare, can_remove) VALUES
-	('cokebrick', 'Brick of coke', -1, 0, 1),
-	('weedbrick', 'Brick of weed', -1, 0, 1),
-	('methbrick', 'Brick of meth', -1, 0, 1)
+DELETE FROM items WHERE name = 'cokebrick';
+DELETE FROM items WHERE name = 'weedbrick';
+DELETE FROM items WHERE name = 'methbrick';
+
+INSERT INTO `items` (name, label, limit) VALUES
+	('cokebrick', 'Brick of coke', -1),
+	('weedbrick', 'Brick of weed', -1),
+	('methbrick', 'Brick of meth', -1)
 ;
